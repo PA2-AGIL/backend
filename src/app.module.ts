7debf182f05fb6ea5be1558719f8acb52620f7d5
Luan de Souza <luandesouzanascimento2011@gmail.com>
2021-11-01 15:52:00 -0300

    adicionado typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
