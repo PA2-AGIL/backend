864a070799cbfe21c9e51a25d08f7cca9b43bfea
Luan de Souza <luandesouzanascimento2011@gmail.com>
2021-11-17 20:06:33 -0300

    chore: exclusão da migration de expert e criação da migration de question

---
+++ src/database/entities/question/question.ts
@@ -0,0 +1,33 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Question extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  closed: boolean;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updated_at: Date;
+}
