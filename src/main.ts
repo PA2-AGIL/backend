bb477afbc5736fd6a2bfbc8919a287210319d28a
Antonio Hauren <haurenburu@protonmail.com>
2021-11-21 11:57:15 -0300

    feat: Add aws sdk configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { config } from 'aws-sdk';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  config.update({
+    accessKeyId: `${process.env.AWS_ACCESS_KEY_ID}`,
+    secretAccessKey: `${process.env.AWS_SECRET_ACCESS_KEY}`,
+    region: `${process.env.AWS_REGION}`,
+  });
 
   await app.listen(3000);
 }
