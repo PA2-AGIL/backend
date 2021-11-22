130e10ded1c527d6b5e5b878ddbef78d0b764249
Antonio Hauren <haurenburu@protonmail.com>
2021-11-22 17:56:35 -0300

    feat: Resize images before send it to s3

--- src/service/file-upload/file-upload.service.ts
+++ src/service/file-upload/file-upload.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { S3 } from 'aws-sdk';
 import { v4 as uuid } from 'uuid';
+import sharp from 'sharp';
 
 @Injectable()
 export class FileUploadService {
   async upload(dataBuffer: Buffer, filename: string) {
+    const resized = await sharp(dataBuffer)
+      .resize({
+        width: 500,
+        height: 500,
+      })
+      .toFormat('jpeg')
+      .toBuffer()
+      .then((o) => o);
+
     const s3 = new S3();
     const uploadResult = await s3
       .upload({
         Bucket: `${process.env.AWS_PUBLIC_BUCKET_NAME}`,
-        Body: dataBuffer,
+        Body: resized,
         Key: `${uuid()}-${filename}`,
+        ContentType: `image/jpg`,
       })
       .promise();
 
