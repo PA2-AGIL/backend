193eb73dc966d41b3ebebc55a4b4db8fa6cab5ff
Luan de Souza <luandesouzanascimento2011@gmail.com>
2021-11-22 14:52:26 -0300

    chore: entidade answer criada

---
+++ src/database/entities/answer/answer.ts
@@ -0,0 +1,30 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Answer extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  content: string;
+
+  @Column({ nullable: false })
+  owner: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updated_at: Date;
+}
