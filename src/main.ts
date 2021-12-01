436ce077b992e577ac32d5e57d58d50a8fcd8b16
Antonio Hauren <haurenburu@protonmail.com>
2021-12-01 12:48:34 -0300

    chore: Add swagger open APi

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { config } from 'aws-sdk';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,15 @@ async function bootstrap() {
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('docs')
+    .setDescription('')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('/docs', app, document);
 
   config.update({
     accessKeyId: `${process.env.AWS_ACCESS_KEY_ID}`,
