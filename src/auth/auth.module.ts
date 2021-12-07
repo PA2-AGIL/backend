9c33bce405abd26681d55fc590c1178da0e99e22
Luan de Souza <luandesouzanascimento2011@gmail.com>
2021-12-07 19:51:29 -0300

    chore: passport jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ExpertModule } from 'src/presentation/expert/expert.module';
 import { ProducerModule } from 'src/presentation/producer/producer.module';
 
 @Module({
-  imports: [ExpertModule, ProducerModule],
+  imports: [
+    ExpertModule,
+    ProducerModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      secret: 'Pa2Agil',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
