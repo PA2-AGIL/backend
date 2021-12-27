4ea10c6426200d09bf385860d8da1d47c6a71338
Luan de Souza <luandesouzanascimento2011@gmail.com>
2021-12-27 14:38:34 -0300

    refact: expert modeule with mongoose module

--- src/presentation/expert/expert.module.ts
+++ src/presentation/expert/expert.module.ts
@@ -1,15 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from 'src/auth/auth.module';
+import { Expert, ExpertSchema } from 'src/database/entities/expert/expert';
 import { ExpertRespository } from 'src/database/repositories/expert.repository';
 import { FileUploadModule } from 'src/service/file-upload/file-upload.module';
 import { ExpertController } from './expert.controller';
 import { ExpertService } from './expert.service';
 
 @Module({
-  imports: [AuthModule, MongooseModule.forFeature([]), FileUploadModule],
+  imports: [
+    AuthModule,
+    MongooseModule.forFeature([
+      {
+        name: Expert.name,
+        schema: ExpertSchema,
+      },
+    ]),
+    FileUploadModule,
+  ],
   controllers: [ExpertController],
-  providers: [ExpertService],
+  providers: [ExpertService, ExpertRespository],
   exports: [ExpertService],
 })
 export class ExpertModule {}
