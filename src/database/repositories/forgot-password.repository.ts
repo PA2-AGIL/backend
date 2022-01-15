686122f8ecd732ed5b3c458b2220c4b606cb333f
Luan de Souza <luandesouzanascimento2011@gmail.com>
2022-01-15 11:09:17 -0300

    refact: path to forgot password

--- src/database/repositories/forgot-password.repository.ts
+++ src/database/repositories/forgot-password.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { ForgotPasswordDTOImp } from 'src/forgot-password/dto/forgotPasswordDTO';
+import { ForgotPasswordDTOImp } from 'src/service/forgot-password/dto/forgotPasswordDTO';
 import { Password } from '../entities/password/password';
 
 @Injectable()
